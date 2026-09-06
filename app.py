1b29a6e3281804db2bf09242103824f287ecee4b
Febriansyah Permana <febriansyhhhh27@gmail.com>
2026-09-06 22:01:35 +0700

    update page_title

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ import streamlit as st
 # layout="wide" supaya dashboard bisa memakai lebar penuh browser.
 # ---------------------------------------------------------
 st.set_page_config(
-    page_title="Dashboard Pencairan",
+    page_title="Dashboard Fasilitas Credit",
     layout="wide",
     initial_sidebar_state="expanded",
 )
