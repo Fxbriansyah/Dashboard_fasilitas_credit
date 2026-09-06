6c7df5897172f3f44456138e08fc45247d3d91e5
Febriansyah Permana <febriansyhhhh27@gmail.com>
2026-09-06 21:41:39 +0700

    update dashboard

--- app.py
+++ app.py
@@ -39,7 +39,6 @@ import streamlit as st
 # ---------------------------------------------------------
 st.set_page_config(
     page_title="Dashboard Pencairan - All Segment Bisnis",
-    page_icon="📊",
     layout="wide",
     initial_sidebar_state="expanded",
 )
